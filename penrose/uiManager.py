c347d98447fc46824d427c7eb3db747e16d7d2ac
Allex Lima <allexlima97@gmail.com>
2017-10-08 10:53:16 -0400

    update

--- penrose/uiManager.py
+++ penrose/uiManager.py
@@ -7,25 +7,31 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 	def __init__(self):
 		super(MainWindow, self).__init__()
 		self.setupUi(self)
+		self.graphs = None
 		self.actions()
 		self.init_canvas()
+		self.create_table()
 
 	def init_canvas(self):
-		self.g1 = PlotCanvas(self)
-		self.g1.move(380 + 30, 37 + 100)
-		self.g2 = PlotCanvas(self)
-		self.g2.move(380 + 325, 37 + 100)
+		self.graphs = [PlotCanvas(self) for i in range(2)]
+		self.graphs[0].move(380 + 30, 37 + 100)
+		self.graphs[1].move(380 + 325, 37 + 100)
 
-	def set_polygon(self, points1, points2):
-		self.g1.plot("Initial polygon", points1)
-		self.g2.plot("Final polygon", points2)
+	def set_polygon(self, points1=None, points2=None):
+		self.graphs[0].plot("Initial polygon", points1)
+		self.graphs[1].plot("Final polygon", points2)
 
 	def actions(self):
 		self.actionOpen_file.setEnabled(False)
 		self.actionSave.setEnabled(False)
+		self.actionClear.setEnabled(False)
+		self.popVertice.setEnabled(False)
 		self.actionExit.triggered.connect(self.close)
 		self.pushVertice.clicked.connect(self.button_addvertice)
 
 	def button_addvertice(self):
 		self.helpText.hide()
 		# self.helpText.show()
+
+	def create_table(self):
+		self.tableVertices.set_template()
--- penrose/uiPlots.py
+++ penrose/uiPlots.py
@@ -16,13 +16,12 @@ class PlotCanvas(FigureCanvas):
 
 	def plot(self, title, points):
 		ax = self.figure.add_subplot(111)
-		if points is None:
-			points = np.array([[0, 0], [0, 0]])
-			ax.cla()
+		ax.cla()
 		ax.set_aspect("equal")
-		tr = plt.Polygon(points)
-		ax.add_patch(tr)
-		ax.relim()
-		ax.autoscale_view()
-		ax.set_title(title)
-		self.draw()
+		if points is not None:
+			tr = plt.Polygon(points)
+			ax.add_patch(tr)
+			ax.relim()
+			ax.autoscale_view()
+			ax.set_title(title)
+			self.draw()
