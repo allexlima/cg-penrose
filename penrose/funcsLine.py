b46b8820b14f207ef9fe6d72251473a72eba8277
Allex Lima <allexlima97@gmail.com>
2017-11-15 14:11:07 -0400

    Finishing 'simple_line' algorithm

--- penrose/funcsLine.py
+++ penrose/funcsLine.py
@@ -1,7 +1,16 @@
+import numpy as np
 
 
-def simple_line():
-	print("Simple Line")
+def simple_line(vertices):
+	(xa, ya, xb, yb) = tuple(np.abs(vertices).reshape(1, 4)[0])
+	m = (yb - ya) / (xb - xa)
+	b = ya - m*xa
+	n_points = [(xa, ya)]
+	while xa < xb:
+		xa += 1
+		y = m*xa + b
+		n_points.append((xa, y))
+	return np.array(n_points, dtype=np.uint8)
 
 
 def basic_incremental():
@@ -10,3 +19,11 @@ def basic_incremental():
 
 def bresenham():
 	print("Bresenham")
+
+
+if __name__ == "__main__":
+	points = np.array([
+		[2, 6],
+		[6, 1]
+	])
+	print(simple_line(points))
